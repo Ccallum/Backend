803e19b90feb144202d50b6c4fa882be6d8330fc
Callum <juan.gamo@email.com>
2022-03-22 14:17:34 +0100

    Version 3.0

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,7 +5,7 @@ const userControllers = require('../controllers/userControllers');
 const router = Router();
 
 router.post('/create', userControllers.createUser);
-router.get('/byEmail/:mail', userControllers.getUserByMail);
+router.get('/byEmail/:mail', userControllers.getUserByEmail);
 router.delete('/delete/:userId', userControllers.deleteUser);
 router.patch('/update/:userId', userControllers.updateUser);
 
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const app = require('./app');
 
 const port = 3000;
-const db = 'mongodb://localhost:27017/API_1';
+const db = 'mongodb://127.0.0.1:27017/API_1';
 
 mongoose.connect(db, (error) => {
   if (error) console.log('Database unable to connect');
