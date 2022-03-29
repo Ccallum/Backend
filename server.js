4e55022bac99837ffd6746c273fb18c7e005e9d9
Callum <juan.gamo@email.com>
2022-03-29 14:03:37 +0200

    4.0

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 const app = require('./app');
 
-const port = 3000;
-const db = 'mongodb://127.0.0.1:27017/API_1';
+const port = process.env.PORT || 3000;
+const db = process.env.MONGODB || 'mongodb://127.0.0.1:27017/API_1';
 
 mongoose.connect(db, (error) => {
   if (error) console.log('Database unable to connect');
